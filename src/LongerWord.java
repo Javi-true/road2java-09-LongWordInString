4ff6d0e82c261f157cf9909e9040fa1b34ddb26d
Javier Trujillo <rivera.dev@outlook.com>
2024-12-08 03:41:03 -0600

    Done.

--- src/LongerWord.java
+++ src/LongerWord.java
@@ -1,7 +1,6 @@
 //importing Scanner class.
 import java.util.Scanner;
 public class LongerWord {
-
     //main method.
     public static void main(String[] args){
 
@@ -11,19 +10,20 @@ public class LongerWord {
     //Asking user to input a sentence.
     System.out.print("Please write a sentence: ");
     String sentence = scan.nextLine();
+    scan.close();
 
     //splitting string in an array.
     String[] split = sentence.split("\\s+");
 
+    //longer word container.
     String longer = "";
 
+    //comparison.
     for (int i = 0; i < split.length; i++){
-
         if (split[i].length() > longer.length() ){
             longer = split[i];
         }
     }
-
     System.out.println(longer);
     }
 }
