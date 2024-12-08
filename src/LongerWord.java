8add38b0bb64f7a3bd5acbb4d6231a96683e19f1
Javier Trujillo <rivera.dev@outlook.com>
2024-12-08 03:30:09 -0600

    main method added

--- src/LongerWord.java
+++ src/LongerWord.java
@@ -1,5 +1,29 @@
+//importing Scanner class.
+import java.util.Scanner;
 public class LongerWord {
+
+    //main method.
     public static void main(String[] args){
 
+    //instancing scanner object.
+    Scanner scan = new Scanner(System.in);
+
+    //Asking user to input a sentence.
+    System.out.print("Please write a sentence: ");
+    String sentence = scan.nextLine();
+
+    //splitting string in an array.
+    String[] split = sentence.split("\\s+");
+
+    String longer = "";
+
+    for (int i = 0; i < split.length; i++){
+
+        if (split[i].length() > longer.length() ){
+            longer = split[i];
+        }
+    }
+
+    System.out.println(longer);
     }
 }
